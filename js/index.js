af406e133dbcb0bb8c07e85e9d7b2d492704fe8b
Asha <ashalbenjamin@gmail.com>
2019-06-06 18:59:50 -0500

    mouseover-eventlisteners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+//Event Listeners---mouseover
+const buttons = document.querySelectorAll('.btn');
+buttons[0].addEventListener('mouseover', function(event) {
+    event.target.style.backgroundColor = 'aqua'
+})
+buttons[1].addEventListener('mouseover', function(event) {
+    event.target.style.backgroundColor = 'yellow'
+})
+buttons[2].addEventListener('mouseover', function(event) {
+    event.target.style.backgroundColor = 'grey'
+})
