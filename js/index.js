f17c94221bb4707e2787ef4be7394ff6189e55d2
Asha <ashalbenjamin@gmail.com>
2019-06-07 08:13:26 -0500

    mouseleave-eventlisteners

--- js/index.js
+++ js/index.js
@@ -1,12 +1,24 @@
 // Your code goes here
-//Event Listeners---mouseover
+
+//Event Listeners---1--mouseover
 const buttons = document.querySelectorAll('.btn');
 buttons[0].addEventListener('mouseover', function(event) {
     event.target.style.backgroundColor = 'aqua'
-})
+});
 buttons[1].addEventListener('mouseover', function(event) {
     event.target.style.backgroundColor = 'yellow'
-})
+});
 buttons[2].addEventListener('mouseover', function(event) {
     event.target.style.backgroundColor = 'grey'
+});
+
+//Event Listeners--2--mouseleave
+buttons[0].addEventListener('mouseleave', function(event) {
+    event.target.style.backgroundColor = 'blue';
+})
+buttons[1].addEventListener('mouseleave', function(event) {
+    event.target.style.backgroundColor = 'orange';
+})
+buttons[2].addEventListener('mouseleave', function(event) {
+    event.target.style.backgroundColor = 'red';
 })
